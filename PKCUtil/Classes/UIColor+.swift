3517457df7daeebafcfcfa35f4a646585bcebc0c
pikachu987 <pikachu77769@gmail.com>
2017-05-22 17:10:00 +0900

    Add HexColor

--- PKCUtil/Classes/UIColor+.swift
+++ PKCUtil/Classes/UIColor+.swift
@@ -33,4 +33,38 @@ public extension UIColor {
     public var greenValue: CGFloat{ return CIColor(color: self).green }
     public var blueValue: CGFloat{ return CIColor(color: self).blue }
     public var alphaValue: CGFloat{ return CIColor(color: self).alpha }
+
+    public var toHexString: String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+
+        return String(
+            format: "%02X%02X%02X",
+            Int(r * 0xff),
+            Int(g * 0xff),
+            Int(b * 0xff)
+        )
+    }
+    convenience init(hexString: String) {
+        let scanner = Scanner(string: hexString)
+        scanner.scanLocation = 0
+
+        var rgbValue: UInt64 = 0
+
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
 }
