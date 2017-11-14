3ac8b6666dd09576e12cab77b85c0bfd54974c5c
pikachu987 <pikachu987@naver.com>
2017-11-14 19:26:40 +0900

    Update

--- PKCUtil/Classes/kString.swift
+++ PKCUtil/Classes/kString.swift
@@ -10,6 +10,10 @@ import UIKit
 
 public class kSTRING {
     public class TITLE{
+        public class DO{
+            public static var MOVE = "이동하기"
+        }
+        
         public static var CONFIRM = "확인"
         public static var CANCEL = "취소"
         public static var DELETE = "삭제"
@@ -37,9 +41,33 @@ public class kSTRING {
         public static var FRIDAY = "금"
         public static var SATURDAY = "토"
         public static var SUNDAY = "일"
-        
-        public class DO{
-            public static var MOVE = "이동하기"
-        }
     }
+    
+    public static func TITLE_DO_MOVE(_ str: String){ kSTRING.TITLE.DO.MOVE = str }
+    public static func TITLE_CONFIRM(_ str: String){ kSTRING.TITLE.CONFIRM = str }
+    public static func TITLE_CANCEL(_ str: String){ kSTRING.TITLE.CANCEL = str }
+    public static func TITLE_DELETE(_ str: String){ kSTRING.TITLE.DELETE = str }
+    public static func TITLE_YES(_ str: String){ kSTRING.TITLE.YES = str }
+    public static func TITLE_MOVE(_ str: String){ kSTRING.TITLE.MOVE = str }
+    public static func TITLE_GENDERM(_ str: String){ kSTRING.TITLE.GENDERM = str }
+    public static func TITLE_GENDERF(_ str: String){ kSTRING.TITLE.GENDERF = str }
+    public static func TITLE_COMPLETE(_ str: String){ kSTRING.TITLE.COMPLETE = str }
+    public static func TITLE_SETTING(_ str: String){ kSTRING.TITLE.SETTING = str }
+    public static func TITLE_REMOVE(_ str: String){ kSTRING.TITLE.REMOVE = str }
+    public static func TITLE_START(_ str: String){ kSTRING.TITLE.START = str }
+    public static func TITLE_EXIT(_ str: String){ kSTRING.TITLE.EXIT = str }
+    public static func TITLE_SAVE(_ str: String){ kSTRING.TITLE.SAVE = str }
+    public static func TITLE_SHARE(_ str: String){ kSTRING.TITLE.SHARE = str }
+    public static func TITLE_CAMERA(_ str: String){ kSTRING.TITLE.CAMERA = str }
+    public static func TITLE_GALLERY(_ str: String){ kSTRING.TITLE.GALLERY = str }
+    public static func TITLE_SEARCH(_ str: String){ kSTRING.TITLE.SEARCH = str }
+    public static func TITLE_MORNING(_ str: String){ kSTRING.TITLE.MORNING = str }
+    public static func TITLE_AFTERNOON(_ str: String){ kSTRING.TITLE.AFTERNOON = str }
+    public static func TITLE_MONDAY(_ str: String){ kSTRING.TITLE.MONDAY = str }
+    public static func TITLE_TUESDAY(_ str: String){ kSTRING.TITLE.TUESDAY = str }
+    public static func TITLE_WEDNESDAY(_ str: String){ kSTRING.TITLE.WEDNESDAY = str }
+    public static func TITLE_THURSDAY(_ str: String){ kSTRING.TITLE.THURSDAY = str }
+    public static func TITLE_FRIDAY(_ str: String){ kSTRING.TITLE.FRIDAY = str }
+    public static func TITLE_SATURDAY(_ str: String){ kSTRING.TITLE.SATURDAY = str }
+    public static func TITLE_SUNDAY(_ str: String){ kSTRING.TITLE.SUNDAY = str }
 }
